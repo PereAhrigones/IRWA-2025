e584b85e29cd7a297eeab8c3845a555ef30f5a74
PereAhrigones <albert.perez06@estudiant.upf.edu>
2025-11-02 17:04:22 +0100

    Cambiado un comentario que estaba mal en la clase document

--- myapp/search/objects.py
+++ myapp/search/objects.py
@@ -21,7 +21,7 @@ class Document(BaseModel):
     average_rating: Optional[float] = None
     url: Optional[str] = None
     images: Optional[List[str]] = None # List of image URLs // Not needed for search
-    line: Optional[str] = None  # Combined field for search indexing // Not needed for search
+    line: Optional[str] = None  # Combined field for search indexing
 
     def to_json(self):
         return self.model_dump_json()
