d851830bf814f3f69f8653c1c305d4a6c4322f1b
PereAhrigones <albert.perez06@estudiant.upf.edu>
2025-12-04 14:01:11 +0100

    Avg rating en results

--- myapp/search/search_engine.py
+++ myapp/search/search_engine.py
@@ -31,13 +31,13 @@ class SearchEngine:
 
         results = []
         ### You should implement your search logic here:
-        best_results = search_appg25(search_query, index, idf, tf, title_index, doc_lengths, corpus)  # replace with call to search algorithm
+        best_results = search_appg25(search_query, index, idf, tf, title_index, doc_lengths, corpus)
 
         # Return all ranked results; pagination will be handled by the web layer
         for doc_id in best_results:
             doc = corpus[doc_id]
             results.append(Document(pid=doc.pid, title=doc.title, description=doc.description, actual_price=doc.actual_price,
-                                    selling_price=doc.selling_price, avg_rating=doc.average_rating, discount=doc.discount,
+                                    selling_price=doc.selling_price, average_rating=doc.average_rating, discount=doc.discount, doc_url=doc.url,
                                     url="doc_details?pid={}&search_id={}&param2=2".format(doc.pid, search_id)))
         # results = search_in_corpus(search_query)
         return results
